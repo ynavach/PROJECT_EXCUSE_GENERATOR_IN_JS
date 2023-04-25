a9c1c2ef33622f5d4951c205bfa2213f98aa8a59
ynavach <yunensey@hotmail.com>
2023-04-25 01:37:13 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+/*import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";*/
+
+function getRandomInt() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  return randomNumber;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your code here//
+  /*console.log("Hello Rigo from the console!");*/
+  let arrayWho = ["The dog", "My granma", "His turtle", "My bird"];
+  let arrayWhat = ["eat", "pissed", "crushed", "broked"];
+  let arrayAvoid = ["my homework", "my T_shirt", "my meal", "my shoes"];
+  let arrayWhen = [
+    "before the class",
+    "during my lunch",
+    "while I was praying",
+    "after dinner"
+  ];
+
+  let randWho = getRandomInt();
+  let randwhat = getRandomInt();
+  let randAvoid = getRandomInt();
+  let randWhen = getRandomInt();
+
+  let theNewExcuse =
+    arrayWho[randWho] +
+    " " +
+    arrayWhat[randwhat] +
+    " " +
+    arrayAvoid[randAvoid] +
+    " " +
+    arrayWhen[randWhen];
+
+  var element = document.getElementById("excuse");
+
+  element.innerText = theNewExcuse;
+
+  return theNewExcuse;
 };
